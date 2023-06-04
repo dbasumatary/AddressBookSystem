065a4167947da6afe2c46b7b0c7112abfaec461d
dbasumatary <basudhondip@gmail.com>
2023-06-05 00:43:06 +0530

    Added: New contacts to address book

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        ArrayList contactList = new ArrayList();
+
+        public Contacts AddContacts()
+        {
+            Contacts contactNew = new Contacts();
+
+            Console.Write("Enter the First Name: ");
+            contactNew.firstName = Console.ReadLine();
+            Console.Write("Enter the Last Name: ");
+            contactNew.lastName = Console.ReadLine();
+            Console.Write("Enter the City: ");
+            contactNew.city = Console.ReadLine();
+            Console.Write("Enter the State: ");
+            contactNew.state = Console.ReadLine();
+            Console.Write("Enter the Address: ");
+            contactNew.address = Console.ReadLine();
+            Console.Write("Enter the Zip Code: ");
+            contactNew.zipcode = Console.ReadLine();
+            Console.Write("Enter the Phone: ");
+            contactNew.phone = Console.ReadLine();
+            Console.Write("Enter the Email: ");
+            contactNew.email = Console.ReadLine();
+
+            contactList.Add(contactNew);
+            return contactNew;
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (Contacts contact in contactList)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Address Book System");
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nPLease enter the choices");
+                Console.WriteLine("\n1. Add New Contact\n2. Display the contacts\n3. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressBook.AddContacts();
+                        break;
+                    case 2:
+                        addressBook.DisplayContacts();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
