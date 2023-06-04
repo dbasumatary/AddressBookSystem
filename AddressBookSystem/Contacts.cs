e5fc1da29d1d1a94e866b7f4c5eb97c064772ad7
dbasumatary <basudhondip@gmail.com>
2023-06-04 12:38:07 +0530

    Added: Program to create contacts in address book

---
+++ AddressBookSystem/Contacts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+
+        public override string ToString()
+        {
+            return $"First Name: {firstName}, Last Name : {lastName}, Address: {address}, City: {city}, State: {state}, ZIP: {zipcode}, Phone: {phone}, Email : {email}";
+        }
+    }
+}
